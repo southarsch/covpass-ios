2de412e9739ba0067d2d01de6c1d76bccd080557
southarsch <70616788+southarsch@users.noreply.github.com>
2021-08-08 14:38:14 +0200

    Update QRCoder.swift
    better exception handling as clearly states technical fault behind exception

--- Source/CovPassCommon/Sources/CovPassCommon/Processing/QRCoder.swift
+++ Source/CovPassCommon/Sources/CovPassCommon/Processing/QRCoder.swift
@@ -21,7 +21,7 @@ enum QRCoder {
             let payload = payload.stripPrefix()
             let base45Decoded = try Base45Coder.decode(payload)
             guard let decompressedPayload = Compression.decompress(Data(base45Decoded)) else {
-                throw ApplicationError.general("Could not decompress QR Code data")
+                throw ApplicationError.general("Could not decode base45 from int casted  QR Code data")
             }
             let cosePayload = try CoseSign1Message(decompressedPayload: decompressedPayload)
             seal.fulfill(cosePayload)
